ff3ceb651bae5260b6d606607dd9aac5ff970e1e
StephanyWinam <stephaniewinam@gmail.com>
2019-02-03 18:08:30 +0300

    Add dynamic routes to template

--- app/views.py
+++ app/views.py
@@ -8,4 +8,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message= 'News Highlights'
+    return render_template('index.html', message=message)
+
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)
