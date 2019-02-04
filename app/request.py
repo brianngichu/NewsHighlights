99ffb1a76f55b2bff82943d6b246d950a1695976
StephanyWinam <stephaniewinam@gmail.com>
2019-02-04 19:46:28 +0300

    Add configuration objects to request.py file

--- app/request.py
+++ app/request.py
@@ -1,4 +1,13 @@
 from app import app
+import urllib.request,json
+from .models import source
+from .models import articles
 
+Source=source.Source
+Articles=articles.Articles
 #Getting api key
 api_key= app.config['NEWS_API_KEY']
+
+
+sources_url=app.config['SOURCES_BASE_URL']
+everything_url=app.config['EVERYTHING_BASE_URL']
