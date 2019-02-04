c61755daf8bcfcb596d26e6242060594ed24973e
StephanyWinam <stephaniewinam@gmail.com>
2019-02-04 09:36:32 +0300

    Add html structure to templates

--- app/views.py
+++ app/views.py
@@ -8,8 +8,8 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message= 'News Highlights'
-    return render_template('index.html', message=message)
+    title= 'Get the latest news'
+    return render_template('index.html', title=title)
 
 
 @app.route('/news/<int:news_id>')
