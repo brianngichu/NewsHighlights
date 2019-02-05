e5f8343f7a021f927e4e6ec739db1502e65bd52a
StephanyWinam <stephaniewinam@gmail.com>
2019-02-05 09:58:32 +0300

    Add function in request.py file to fetch results

--- app/request.py
+++ app/request.py
@@ -11,3 +11,23 @@ api_key= app.config['NEWS_API_KEY']
 
 sources_url=app.config['SOURCES_BASE_URL']
 everything_url=app.config['EVERYTHING_BASE_URL']
+
+
+def get_source(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_source_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_source_url) as url:
+        get_source_data = url.read()
+        get_source_response = json.loads(get_source_data)
+
+        source_results = None
+
+        if get_source_response['sources']:
+            source_results_list = get_source_response['sources']
+            source_results = process_results(source_results_list)
+
+
+    return source_results
