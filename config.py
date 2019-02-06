238c1793706686bffad08d7a62f7e05530f8c642
StephanyWinam <stephaniewinam@gmail.com>
2019-02-06 12:53:55 +0300

    Move config.py file to root directory and import os module

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
     SOURCES_BASE_URL='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     EVERYTHING_BASE_URL='https://newsapi.org/v2/everything?sources={}&apiKey={}'
-
+    NEWS_API_KEY= os.environ.get('NEWS_API_KEY')
+    SECRET_KEY=os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
@@ -26,3 +28,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+    config_options={
+        'development':DevConfig,
+        'production':ProdConfig
+    }
